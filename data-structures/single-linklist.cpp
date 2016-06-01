76f48e72c09506976be7abe214281a094ed47f7e
Shubham Sahai <ssahai@cse.iitk.ac.in>
2016-06-02 04:03:10 +0530

    initilized code for single link list

--- data-structures/single-linklist.cpp
+++ data-structures/single-linklist.cpp
@@ -1,13 +1,25 @@
 #include <iostream>
+#include <cstdlib>
 
 using namespace std;
 
 typedef struct node {
     int data;
-    struct *node next;
+    struct node * next;
 }NODE;
 
+NODE * init() {
+    NODE * head = (NODE*) malloc (sizeof (NODE));
+    head -> next = NULL;
+    head -> data = 10;
+    return head;
+}
+
 int main() {
-    cout << "Single - Link List";
+    cout << "Single - Link List"<< endl;
+    cout << "==================";
+    NODE * head;
+    head = init();
+    cout << head->data;
 }
 
